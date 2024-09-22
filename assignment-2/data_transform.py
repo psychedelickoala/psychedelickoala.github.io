e2428ea90f49b8489c5ba78b49cec74d840ee065
psychedelickoala <77258902+psychedelickoala@users.noreply.github.com>
2024-09-22 17:11:47 +1000

    starting assignment 2

--- assignment-2/data_transform.py
+++ assignment-2/data_transform.py
@@ -2,11 +2,29 @@ import numpy as np
 import csv
 from scipy.interpolate import interpn
 
-readfile = "tas_annual.csv"
-writefile = 'fixed_data.csv'
+# add 19 for end year
+files = {
+    "15": 2015,
+    "20": 2020,
+    "25": 2025,
+    "30": 2030,
+    "35": 2035,
+    "40": 2040,
+    "45": 2045,
+    "50": 2050,
+    "55": 2055,
+    "60": 2060,
+    "65": 2065,
+    "70": 2070,
+    "75": 2075,
+    "80": 2080
+}
+
+readfile = "assignment-2/temp-data/tas_annual_15.csv"
+writefile = 'assignment-2/temp-data/processed_tas_annual.csv'
 australia_only = True
-precision = 20
-#lon_ratio = 2 if not australia_only else int((155-111)/(45-11))
+aus_lats = (-45, -9)
+aus_lons = (110, 156)
 
 array = np.loadtxt(readfile, delimiter=",")
 
@@ -19,11 +37,10 @@ points = (latitudes, longitudes)
 values = array
 
 
-new_lats = np.linspace(start=-90, stop=90, num=lats*precision, endpoint=True)
-new_lons = np.linspace(start=0, stop=360, num=lons*precision, endpoint=True)
+new_lats = np.linspace(start=-90, stop=90, num=lats*4, endpoint=True)
+new_lons = np.linspace(start=0, stop=360, num=lons*7, endpoint=True)
 
 coords = np.array(np.meshgrid(new_lats,new_lons)).transpose([1,2,0])
-#print(coords)
 
 interpolation = interpn(points, values, coords)
 interpolation = interpolation.transpose([1, 0])
@@ -36,23 +53,5 @@ with open(writefile, 'w', newline='') as csvfile:
 
     for i in range(len(new_lats)):
         for j in range(len(new_lons)):
-            if not australia_only or (-45 <= new_lats[i] <= -11 and 111 <= new_lons[j] <= 155):
-                writer.writerow([new_lats[i], new_lons[j], interpolation[i, j]])
-
-
-# cemetery
-"""
-    i_min, j_min = i//precision, j//(precision*lon_ratio)
-    i_max, j_max = i_min + 1, j_min + 1
-    if i_max >= lats:
-        i_max =  i_min
-    if j_max >= lons:
-        j_max =  j_min
-    
-    ll, lr, ul, ur = array[i_min, j_min], array[i_min, j_max], array[i_max, j_min], array[i_max, j_max]
-    h_frac = (i%precision)/precision
-    v_frac = (j%(lon_ratio*precision))/(lon_ratio*precision)
-    return (h_frac*ll + (1-h_frac)*lr)*v_frac + (h_frac*ul + (1-h_frac)*ur)*(1-v_frac)
-
-
-"""
+            if not australia_only or (aus_lats[0] <= new_lats[i] <= aus_lats[1] and aus_lons[0] <= new_lons[j] <= aus_lons[1]):
+                writer.writerow([np.round(new_lats[i], 3), np.round(new_lons[j], 3), np.round(interpolation[i, j], 3)])
